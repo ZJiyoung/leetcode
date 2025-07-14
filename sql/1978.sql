2e6613e0093063643d0894b53ead5a3d1ae33e5e
ZJiyoung <kimdanchuchu@gmail.com>
2025-07-14 18:33:27 +0900

    Subqueries

--- sql/1978.sql
+++ sql/1978.sql
@@ -7,3 +7,11 @@ WHERE e1.salary < 30000
   AND e1.manager_id IS NOT NULL
   AND e2.employee_id IS NULL
 ORDER BY e1.employee_id;
+
+# Subqueries
+SELECT employee_id
+FROM Employees
+WHERE salary < 30000
+  AND manager_id IS NOT NULL
+  AND manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id;
